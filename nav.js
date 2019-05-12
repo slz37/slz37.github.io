275e8d06ddd3fb7cd7f83d0d21ff42401845be53
slz37 <slz37@drexel.edu>
2019-05-11 20:42:52 -0400

    Added transition affects between pages

--- nav.js
+++ nav.js
@@ -1,26 +1,44 @@
 function loadDiv(event, link) {
 	/*
-	Load the html contents of the page selected by the 
-	user into the content div.
+	Transitions the div from the previous page
+	to the new page with fade out/in affects.
 	*/
 	
 	//Get div
 	var content_div = document.getElementById("content_div");
 	
+	//Change class to fade out transition
+	content_div.className = 'hide';
+	
+	//Wait until transition and then change content
+	window.setTimeout(setContent, 2000, event, link);
+	
+	//Wait for transition and change class back to fade in
+	window.setTimeout(function() {content_div.className = 'show'}, 2000);
+	
+	//Update nav bar to show selected item
+	swapClasses(event);
+}
+
+function setContent(event, link) {
+	/*
+	Sets the content of the div to the content of the selected
+	page.
+	*/
+	
 	//Check if we are loading a pdf instead of html
 	if (link.indexOf(".pdf") !== -1){
+		//Set contents
 		content_div.innerHTML = `<iframe src="${link}" style="border:0px #FFFFFF none;" 
 									name="CV" scrolling="no" frameborder="1" 
 									marginheight="0px" marginwidth="0px" height="1195" 
 									width="100%"></iframe>`;
 	}
 	else {
+		//Set contents
 		 content_div.innerHTML = `<iframe width="100%" height="100%" 
 										frameborder="0" src="${link}"></iframe>`;
 	}
-	
-	//Update nav bar to show selected item
-	swapClasses(event);
 }
 
 function swapClasses(event) {
