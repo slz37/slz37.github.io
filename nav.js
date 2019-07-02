65bb28959efe2412dd733edcb553655665cd3b0b
Salvatore Zerbo <slz37@drexel.edu>
2019-07-02 03:46:58 -0400

    Fixed div resizing and updated about me

--- nav.js
+++ nav.js
@@ -1,4 +1,4 @@
-function loadDiv(event, link) {
+function loadDiv(ev, page) {
 	/*
 	Transitions the div from the previous page
 	to the new page with fade out/in affects.
@@ -11,53 +11,55 @@ function loadDiv(event, link) {
 	content_div.className = 'hide';
 	
 	//Wait until transition and then change content
-	window.setTimeout(setContent, 1000, event, link);
+	window.setTimeout(setContent, 1000, page, content_div);
 	
 	//Wait for transition and change class back to fade in
 	window.setTimeout(function() {content_div.className = 'show'}, 1000);
 	
 	//Update nav bar to show selected item
-	swapClasses(event);
+	swapClasses(ev);
 }
 
-function setContent(event, link) {
+function setContent(page, content_div) {
 	/*
 	Sets the content of the div to the content of the selected
 	page.
 	*/
-	
-	//Check if we are loading a pdf instead of html
-	if (link.indexOf(".pdf") !== -1){
-		//Set contents
-		content_div.innerHTML = `<iframe src="${link}" style="border:0px #FFFFFF none;" 
+
+	//Check if loaded page is a pdf file
+	if (page.indexOf(".pdf") !== -1){
+		/*
+		Hardcoded height for now. To do: make this automatic
+		for any PDF file in the future.
+		*/
+		
+		content_div.innerHTML = `<iframe src="${page}"
+									id="pdfFrame"
+									style="border:0px #FFFFFF none;" 
 									name="CV" scrolling="no" frameborder="1" 
-									marginheight="0px" marginwidth="0px" height="1195" 
+									marginheight="0px" marginwidth="0px" 
+									height="2324px"
 									width="100%"></iframe>`;
 	}
-	else {
-		//Set contents
-		 content_div.innerHTML = `<iframe width="100%" height="100%" 
-										frameborder="0" src="${link}"></iframe>`;
+	else{
+		$(content_div).load(page);
 	}
 }
 
-function swapClasses(event) {
+function swapClasses(ev) {
 	/*
 	Swap the navigation button classes
 	to select the new one.
 	*/
 	
-	//Start with old
 	document.getElementsByClassName("current")[0].className = "not_current";
-	
-	//Now update new
-	document.getElementById(event).className = "current";
+	document.getElementById(ev).className = "current";
 }
 
 function clearDiv() {
 	/*
 	When the user selects to return to the homepage, clear
-	the content div of all inner html. Currently unused.
+	the content div of all inner html. Deprecated.
 	*/
 	
 	//Get div
